4324fe6e1f74782d5e5d16a70a5b2221ec95fefb
Tian Feng <tian.feng@saucelabs.com>
2022-01-17 10:49:15 -0800

    [DEVX-1442] Add docker example

--- examples/README.md
+++ examples/README.md
@@ -5,3 +5,4 @@ Here we have a list additional examples that cover more complicated setups.
 | Example | Description |
 | ------- | ----------- |
 | [typescript](typescript) | Showcases how to run tests using testcafe with typescript |
+| [docker](docker) | Showcases how to run tests in docker mode |
---
+++ examples/docker/tests/example.test.js
@@ -0,0 +1,32 @@
+// This test is from TestCafe example
+// https://github.com/DevExpress/testcafe-examples/tree/8f4a2e3556d410d8ffe865bf06b9b887b79b7d0e/examples/change-element-style
+import { ClientFunction, Selector } from 'testcafe';
+
+fixture `Change Element Style`
+    .page `https://devexpress.github.io/testcafe/example`;
+
+test('Hide an element', async t => {
+    const populateButton     = Selector('#populate');
+    const hidePopulateButton = ClientFunction(() => {
+        document.getElementById('populate').style.display = 'none';
+    });
+
+    await t.expect(populateButton.visible).ok();
+
+    await hidePopulateButton();
+
+    await t.expect(populateButton.visible).notOk();
+});
+
+test('Change header color', async t => {
+    const header            = Selector('h1');
+    const removeHeaderColor = ClientFunction(() => {
+        document.querySelector('h1').style.color = '#111';
+    });
+
+    await t.expect(header.getStyleProperty('color')).eql('rgb(47, 164, 207)');
+
+    await removeHeaderColor();
+
+    await t.expect(header.getStyleProperty('color')).eql('rgb(17, 17, 17)');
+});
